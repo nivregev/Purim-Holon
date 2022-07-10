c75563ad4cbead032628139417bfffbdf7f5e8da
nivregev <nivregev2@gmail.com>
2022-07-10 18:03:09 +0300

    media photos are ready with js

--- media.js
+++ media.js
@@ -1,6 +1,10 @@
 document.addEventListener("click", function (e) {
   if (e.target.classList.contains("gallery-item")) {
     const src = e.target.getAttribute("src");
-    console.log(src);
+    document.querySelector(".modal-img").src = src;
+    const myModal = new bootstrap.Modal(
+      document.getElementById("gallery-modal")
+    );
+    myModal.show();
   }
 });
